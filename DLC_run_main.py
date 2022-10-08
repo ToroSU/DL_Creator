3fcb9a3a79ea9375edf0de949bd5801c3095355e
Wei-Su <a26513638.gmail.com>
2022-10-08 21:56:50 +0800

    Refactor: fix comment

--- DLC_run_main.py
+++ DLC_run_main.py
@@ -22,9 +22,9 @@ def list_diff(li1, li2):
 
 start = time.time()
 ## Main
-# TO DO: GUI creat config.ini 
-dir_path = os.getcwd() # get current path (as know as driver_package path)
-list_info, os_info, date_source, other_setting, wlanbt_info = DLC_config_reader_main() # 
+# TODO: GUI creat config.ini 
+dir_path = os.getcwd() # get current path (as know as driver package path)
+list_info, os_info, date_source, other_setting, wlanbt_info = DLC_config_reader_main()
 
 
 # Serach all folder at root_folder # Use folder to detect.
@@ -46,12 +46,12 @@ batch_in_folder_path_list, AUMIDs_in_folder_path_list = DLC_info_catch.batch_and
 
 ## Analyze the folder name to choose catch method, then create item list of driver list.
 # We focus on WLANBT folder now. (Cause this item will add another item in driver list)
-# TODO VGA, Liteon WLANBT, MTK WLANBT 
+# TODO VGA, Azwave_Realtek , Liteon_Realtek , Liteon_Qualcomm 
 wlanbt_path_list = DLC_info_catch.wlanbt_analysis(batch_in_folder_path_list) # output format: [[intel wlan], [intel bt], [mtk wlan],
                                                                              #                 [mtk bt], [liteon wlan], [liteon bt]]
 
 # Detect wlanbt module (e.g ax201, ax211) amount and wlanbt folder amount
-# If wlan/bt folder of each vendor has different quantities, will create a Warning message : wlan bt Folder maybe had erred
+# If wlan/bt folder of each vendor has different quantities, program will create a Warning message : wlan bt Folder maybe had erred
 # item_list: list all item which need be in "Category column"
 item_list, item_list_path, aumids_path_list= DLC_info_catch.wlanbt_item_add(batch_in_folder_path_list, AUMIDs_in_folder_path_list, wlanbt_path_list, wlanbt_info)
 
@@ -60,11 +60,11 @@ for i in range(0, len(wlanbt_path_list), 2):
         print("WARNING: Folder {}, WLAN and BT path maybe had erred!".format(wlanbt_path_list[i][0].split("\\")[0]))
 
 
-## Obtain all list, ready for output to excel.
+## Obtain all list for final output.
 print("\n-------------v Message Text v-------------\n")
 all_list = DLC_info_catch.all_List_get(item_list, item_list_path, aumids_path_list, os_info)
 
-## Main program done and output Excel file.
+## Output to Excel file.
 if str2bool(other_setting[0]):
     creat_list(list_info, os_info, item_list, all_list) # input(list_info, os_info, <item_list>)
     print("\nFile output completed, path is: {}".format(dir_path))
