04c9b73a84e3a36ffe93fd75cb941fe52d40c795
ToroSU <a26513638@gmail.com~>
2024-05-13 10:31:35 +0800

    Add: appName intelligo UI rule

--- DLC_info_catch.py
+++ DLC_info_catch.py
@@ -354,6 +354,8 @@ def appName_list_get(item_list_path, aumids_path_list):
     appName_check_list_RealtekConsole = ["realtek", "rtk"]
     appName_check_list_ICPS = ["icps"]
     appName_check_list_Dolby = ["dolby"]
+    appName_check_list_Intelligo = ["intelligo", "inteligo", "igo"]
+
 
     for i in range(0, len(item_list_path)):
         path_split, folder_root_name, name_split = name_split_get(item_list_path[i])
@@ -375,6 +377,10 @@ def appName_list_get(item_list_path, aumids_path_list):
 
                 elif name_split[j] in appName_check_list_Dolby:
                     appName_str = "Dolby Access"
+                    break
+
+                elif name_split[j] in appName_check_list_Intelligo:
+                    appName_str = "ASUS AI Noise-canceling"
                     break
 
                 else:
